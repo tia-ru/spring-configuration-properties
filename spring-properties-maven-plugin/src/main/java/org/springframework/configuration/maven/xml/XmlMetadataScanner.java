d4bbf8e02fc0918a9f3d9f868fdf0d7115ec7cf1
Ilya Tugushev <itugushev@gmail.com>
2024-09-25 17:15:48 +0300

    Replace XML parser

--- spring-properties-maven-plugin/src/main/java/org/springframework/configuration/maven/xml/XmlMetadataScanner.java
+++ spring-properties-maven-plugin/src/main/java/org/springframework/configuration/maven/xml/XmlMetadataScanner.java
@@ -1,9 +1,11 @@
 package org.springframework.configuration.maven.xml;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -13,11 +15,12 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.configurationprocessor.helpers.PropertyPlaceholderHelper;
 import org.springframework.configurationprocessor.helpers.StringUtils;
 import org.springframework.configurationprocessor.metadata.ItemMetadata;
-import xmlparser.XmlParser;
-import xmlparser.model.XmlElement;
 
 public class XmlMetadataScanner {
 
@@ -26,7 +29,6 @@ public class XmlMetadataScanner {
     private static final PropertyPlaceholderHelper PROPERTY_HELPER = new PropertyPlaceholderHelper("${", "}", VALUE_SEPARATOR, true);
 
     private final List<Path> locations;
-    private final XmlParser parser = new XmlParser();
 
     public XmlMetadataScanner(List<Path> locations){
         this.locations = locations;
@@ -101,7 +103,13 @@ public class XmlMetadataScanner {
         }
         return fileMetadata;
     }*/
-    private Set<ItemMetadata> getFileMetadata(Path path) {
+
+    /**
+     * Use simplexml
+     * @param path
+     * @return
+     */
+    /*private Set<ItemMetadata> getFileMetadata(Path path) {
         Set<ItemMetadata> fileMetadata = new HashSet<>();
         LinkedBlockingQueue<XmlElement> queue = new LinkedBlockingQueue<>();
         XmlElement el;
@@ -112,19 +120,49 @@ public class XmlMetadataScanner {
             while ((el = queue.poll()) != null) {
                 if (el instanceof XmlElement.XmlTextElement) {
                     String value = ((XmlElement.XmlTextElement) el).text;
-                    fileMetadata.addAll( extractMeta(value, path));
+                    fileMetadata.addAll(extractMeta(value, path));
                 }
 
                 if (el.attributes != null) {
                     for (String value : el.attributes.values()) {
-                        fileMetadata.addAll( extractMeta(value, path));
+                        fileMetadata.addAll(extractMeta(value, path));
                     }
                 }
 
-                if (el.children != null) queue.addAll(el.children);
+                if (el.children != null)
+                    queue.addAll(el.children);
             }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        } catch (IOException | InvalidXml e){
+            //XML contains non-whitespace characters before opening tag
+            throw new RuntimeException("Unable to process file '" + path +"'. " + e.getMessage(), e);
+        }
+        return fileMetadata;
+    }*/
+
+    private Set<ItemMetadata> getFileMetadata(Path path) {
+        Set<ItemMetadata> fileMetadata = new HashSet<>();
+        LinkedBlockingQueue<Xpp3Dom> queue = new LinkedBlockingQueue<>();
+        Xpp3Dom el;
+
+        try {
+            BufferedReader reader = Files.newBufferedReader(path);
+            Xpp3Dom root =  Xpp3DomBuilder.build(reader);
+            queue.offer(root);
+            while ((el = queue.poll()) != null) {
+
+                if (StringUtils.hasText(el.getValue())) {
+                    fileMetadata.addAll(extractMeta(el.getValue(), path));
+                }
+
+                for (String name : el.getAttributeNames()) {
+                    String value = el.getAttribute(name);
+                    fileMetadata.addAll(extractMeta(value, path));
+                }
+
+                queue.addAll(Arrays.asList(el.getChildren()));
+            }
+        } catch (IOException | XmlPullParserException e){
+            throw new RuntimeException("Unable to process file '" + path +"'. " + e.getMessage(), e);
         }
         return fileMetadata;
     }
